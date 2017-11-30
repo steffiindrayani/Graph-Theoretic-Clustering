72e3111a3890a32c28aa12a9fe2a89075ead6ccc
Steffi Indrayani <13514063@std.stei.itb.ac.id>
2017-11-30 19:28:42 +0700

    normalized;

--- Graph-Theoretic-Clustering.py
+++ Graph-Theoretic-Clustering.py
@@ -5,8 +5,8 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import minimum_spanning_tree, connected_components
 
 #Read Dataset
-trainingdata = pd.read_csv('CensusIncome/lala', header = None)
-testdata = pd.read_csv('CensusIncome/CencusIncome.test.txt', header = None)
+trainingdata = pd.read_csv('CensusIncome_Normalize/CencusIncome.data.csv', header = None)
+testdata = pd.read_csv('CensusIncome_Normalize/CencusIncome.test.csv', header = None)
 
 
 numericattributes = [0,2,4,10,11,12]
@@ -16,7 +16,8 @@ classattributes = 14
 train = trainingdata
 
 trainingdata = trainingdata.as_matrix()
-distancematrix = [[-999 for x in range(len(trainingdata))] for y in range(len(trainingdata))]
+testdata = testdata.as_matrix()
+distancematrix = np.zeros(shape(len(trainingdata), len(trainingdata)))
 
 def countDistance(x, y):
 	sum = 0
@@ -44,26 +45,32 @@ for j in numericattributes:
 	for i in range(0, len(trainingdata)):
 		if range1 != 0 :
 			trainingdata[i][j] = float(trainingdata[i][j] - min1)/float(range1)
+	for i in range(0, len(testdata)):
+		if range1 != 0:
+			testdata[i][j] = float(testdata[i][j] - min1)/float(range1)
+
 
 #MST
+
+
 distancematrix = buildDistanceMatrix()
 distancematrix = csr_matrix(distancematrix)
 MST = minimum_spanning_tree(distancematrix)
 
-#Remove maximum distance
-idxMax = np.unravel_index(MST.argmax(), MST.shape)
-MST[idxMax] = 0
+# #Remove maximum distance
+# idxMax = np.unravel_index(MST.argmax(), MST.shape)
+# MST[idxMax] = 0
 
-mst = MST
-#Label connected components.
-num_graphs, labels = connected_components(mst, directed=False)
+# mst = MST
+# #Label connected components.
+# num_graphs, labels = connected_components(mst, directed=False)
 
-# We should have two trees.
-assert(num_graphs == 2)
+# # We should have two trees.
+# assert(num_graphs == 2)
 
-# Use indices as node ids and group them according to their graph.
-results = [[] for i in range(max(labels) + 1)]
-for idx, label in enumerate(labels):
-    results[label].append(idx)
+# # Use indices as node ids and group them according to their graph.
+# results = [[] for i in range(max(labels) + 1)]
+# for idx, label in enumerate(labels):
+#     results[label].append(idx)
 
-print(results)
+# print(results)
--- Preprocess-Data.py
+++ Preprocess-Data.py
@@ -5,8 +5,13 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import minimum_spanning_tree, connected_components
 
 #Read Dataset
-trainingdata = pd.read_csv('CensusIncome/CencusIncome.data.txt', header = None)
-testdata = pd.read_csv('CensusIncome/CencusIncome.test.txt', header = None)
+trainingdata = pd.read_csv('CensusIncome_Normalize/CencusIncome.data.txt', header = None)
+testdata = pd.read_csv('CensusIncome_Normalize/CencusIncome.test.txt', header = None)
+
+train = trainingdata
+
+trainingdata = trainingdata.as_matrix()
+testdata = testdata.as_matrix()
 
 numericattributes = [0,2,4,10,11,12]
 nominalattributes = [1,3,5,6,7,8,9,13]
@@ -24,10 +29,10 @@ for j in numericattributes:
 		if range1 != 0:
 			testdata[i][j] = float(testdata[i][j] - min1)/float(range1)
 
-with open('CensusIncome_Normalize/CencusIncome.data.txt') as f:
-	trainingdata = numpy.asarray(trainingdata)
-	numpy.savetxt(f, a, delimiter=",")
+print(trainingdata)
+print(testdata)
 
-with open('CensusIncome_Normalize/CencusIncome.test.txt') as f:
-	trainingdata = numpy.asarray(trainingdata)
-	numpy.savetxt(f, a, delimiter=",")
+trainingdata = np.array(trainingdata)
+
+# with open('CensusIncome_Normalize/CencusIncome.test1.txt', 'w') as f:
+# 	np.savetxt(f, trainingdata, delimiter=",")
