fbb2a3c5adbb75caeb35223110fb064c6d6e24ab
Steffi Indrayani <13514063@std.stei.itb.ac.id>
2017-12-01 10:55:34 +0700

    add

--- Graph-Theoretic-Clustering.py
+++ Graph-Theoretic-Clustering.py
@@ -5,19 +5,48 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import minimum_spanning_tree, connected_components
 
 #Read Dataset
-trainingdata = pd.read_csv('CensusIncome_Normalize/CencusIncome.data.csv', header = None)
-testdata = pd.read_csv('CensusIncome_Normalize/CencusIncome.test.csv', header = None)
-
-
+trainingdata = pd.read_csv('CensusIncome/CencusIncome.data.txt', header = None)
+testdata = pd.read_csv('CensusIncome/CencusIncome.test.txt', header = None)
 numericattributes = [0,2,4,10,11,12]
 nominalattributes = [1,3,5,6,7,8,9,13]
 classattributes = 14
 
-train = trainingdata
 
-trainingdata = trainingdata.as_matrix()
-testdata = testdata.as_matrix()
-distancematrix = np.zeros(shape(len(trainingdata), len(trainingdata)))
+for i in nominalattributes:
+	for j in range(len(trainingdata)):
+		if (trainingdata.iloc[j,i].find('?') > -1):
+			trainingdata.iloc[j,i] = trainingdata.mode()[i][0]
+	for j in range(len(testdata)):
+		if (testdata.iloc[j,i].find('?') > -1): testdata.iloc[j,i] = trainingdata.mode()[i][0]
+	
+print(trainingdata.iloc[[149]])
+
+
+# train = trainingdata
+
+# trainingdata = trainingdata.as_matrix()
+# testdata = testdata.as_matrix()
+# distancematrix = np.zeros(shape(len(trainingdata), len(trainingdata)))
+
+
+
+# #normalize:
+# for j in numericattributes:
+# 	max1 = train[j].max()
+# 	min1 = train[j].min()
+# 	range1 = max1 - min1
+
+# 	for i in range(0, len(trainingdata)):
+# 		if range1 != 0 :
+# 			trainingdata[i][j] = float(trainingdata[i][j] - min1)/float(range1)
+# 	for i in range(0, len(testdata)):
+# 		if range1 != 0:
+# 			testdata[i][j] = float(testdata[i][j] - min1)/float(range1)
+
+df = pd.DataFrame(np.asarray(trainingdata))
+df.to_csv("file_path.csv")
+
+
 
 def countDistance(x, y):
 	sum = 0
@@ -36,26 +65,9 @@ def buildDistanceMatrix():
 				distancematrix[i][j] = countDistance(trainingdata[i], trainingdata[j])
 	return distancematrix
 
-#normalize:
-for j in numericattributes:
-	max1 = train[j].max()
-	min1 = train[j].min()
-	range1 = max1 - min1
-
-	for i in range(0, len(trainingdata)):
-		if range1 != 0 :
-			trainingdata[i][j] = float(trainingdata[i][j] - min1)/float(range1)
-	for i in range(0, len(testdata)):
-		if range1 != 0:
-			testdata[i][j] = float(testdata[i][j] - min1)/float(range1)
-
-
-#MST
-
-
-distancematrix = buildDistanceMatrix()
-distancematrix = csr_matrix(distancematrix)
-MST = minimum_spanning_tree(distancematrix)
+# distancematrix = buildDistanceMatrix()
+# distancematrix = csr_matrix(distancematrix)
+# MST = minimum_spanning_tree(distancematrix)
 
 # #Remove maximum distance
 # idxMax = np.unravel_index(MST.argmax(), MST.shape)
